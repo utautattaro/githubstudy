81d983dfc445c571c8db8d38fec1e2ad6683ad0c
hyper-v <tsudaryo@utautattaro.com>
2023-06-26 15:19:58 +0900

    edit python code

--- update_html.py
+++ update_html.py
@@ -1,12 +1,15 @@
 from github import Github
 import os
+import datetime
+
+dt_now = datetime.datetime.now()
 
 g = Github(os.getenv("GH_TOKEN"))
 repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
 commit = repo.get_commit(os.getenv("COMMIT_SHA"))
 message = commit.commit.message
 
-path = "/log/log.txt"
+path = dt_now + ".md"
 
 f = open(path,'w')
 f.write(message)
