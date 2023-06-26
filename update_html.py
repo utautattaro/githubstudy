1399640c110098989de085fa34f2cc753f478bc2
hyper-v <tsudaryo@utautattaro.com>
2023-06-26 16:02:04 +0900

    edit scheme

--- update_html.py
+++ update_html.py
@@ -8,9 +8,10 @@ g = Github(os.getenv("GH_TOKEN"))
 repo = g.get_repo(os.getenv("GITHUB_REPOSITORY"))
 commit = repo.get_commit(os.getenv("COMMIT_SHA"))
 message = commit.commit.message
-now = str(dt_now).split()[0]
+now = str(dt_now)
 now = now.replace(" ","-")
 now = now.replace(":","-")
+now = now.replace(".","-")
 
 path = now + ".md"
 
